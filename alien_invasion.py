48dd9f08ed125ab33af42ff09de524ea000dddff
Frank Mirando <mirandofrank@csu.fullerton.edu>
2019-09-25 15:22:08 -0700

    Update alien_invasion.py
    Removed unnecessary comment

--- alien_invasion.py
+++ alien_invasion.py
@@ -43,4 +43,3 @@ def run_game():
         gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 run_game()
 
-#Left off on page 285 - responding to alien-ship collisions
